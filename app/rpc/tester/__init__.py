8f01fec4d047a43614517289953812aabb1d844d
David Carmichael <carmichaelits@gmail.com>
2024-04-13 11:28:13 +0100

    feat: add string function

--- app/rpc/tester/__init__.py
+++ app/rpc/tester/__init__.py
@@ -1,10 +1,10 @@
 from flask import Blueprint
 
 from flask_rpc.latest import RPC
-from .funcs import *
+from .funcs import add_numbers, add_string
 
 bp = Blueprint("tester", __name__, url_prefix="/tester")
 
 rpc = RPC(bp)
 
-rpc.functions(add_numbers=add_numbers)
+rpc.functions(add_numbers=add_numbers, add_string=add_string)
--- app/rpc/tester/funcs/__init__.py
+++ app/rpc/tester/funcs/__init__.py
@@ -3,18 +3,9 @@ from flask_rpc.latest import RPCResponse as Res
 
 # accept list return int
 def add_numbers(data: list):
-    return Res.success(
-        data=sum(data),
-        message="add_numbers"
-    )
+    return Res.success(data=sum(data), message="add_numbers")
 
 
 # accept string return string
 def add_string(data: str):
-    return Res.success(
-        data=f"Hello {data}!",
-        message="add_string"
-    )
-
-
-__all__ = ["add_numbers"]
+    return Res.success(data=f"Hello {data}!", message="add_string")
