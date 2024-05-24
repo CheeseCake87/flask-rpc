fafca5f96afe147fceb49889b5cb84dfabe72778
David Carmichael <carmichaelits@gmail.com>
2024-05-24 10:35:41 +0100

    feat: Add testing for new validation

--- app/rpc/tester/funcs/__init__.py
+++ app/rpc/tester/funcs/__init__.py
@@ -1,11 +1,23 @@
+from flask_rpc.exceptions import DataException
 from flask_rpc.latest import RPCResponse as Res
+from flask_rpc.validation import DataList, DataStr
 
 
 # accept list return int
 def add_numbers(data: list):
+    try:
+        DataList(data)
+    except DataException:
+        return Res.fail("Invalid data type.", {"data": "list"})
+
     return Res.success(data=sum(data), message="add_numbers")
 
 
 # accept string return string
 def add_string(data: str):
+    try:
+        DataStr(data)
+    except DataException:
+        return Res.fail("Invalid data type.", {"data": "str"})
+
     return Res.success(data=f"Hello {data}!", message="add_string")
