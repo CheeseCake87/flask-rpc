f42dba09a44524eae7ce0e7c76c9eebd3e20539e
David Carmichael <carmichaelits@gmail.com>
2024-06-19 07:41:39 +0100

    feat: add example of scoped auth

--- app/rpc/tester/__init__.py
+++ app/rpc/tester/__init__.py
@@ -1,6 +1,6 @@
 from flask import Blueprint
 
-from flask_rpc.latest import RPC
+from flask_rpc.latest import RPC, RPCAuthSessionKey
 from .funcs import add_numbers, add_string
 
 bp = Blueprint("tester", __name__, url_prefix="/tester")
@@ -11,3 +11,13 @@ rpc.functions(
     add_string=add_string,
 )
 rpc.functions_auto_name((add_numbers,))
+
+rpc.functions(
+    session_auth__=RPCAuthSessionKey("logged_in", [True]),
+    add_string_auth_session=add_string
+)
+
+rpc.functions(
+    host_auth__=["127.0.0.1:5000"],
+    add_string_auth_host=add_string
+)
